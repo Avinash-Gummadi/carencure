903cf59a959a24a058a8286895d8b0ca7a141380
Sai Kumar Avu <saikumaravu18bcs@iiitkottayam.ac.in>
2022-11-28 18:53:03 +0530

    religion added

--- js/scripts.js
+++ js/scripts.js
@@ -51,6 +51,7 @@ function sendEmail(){
         account : document.getElementById("account").value,
         ifsc : document.getElementById("ifsc").value,
         branch : document.getElementById("branch").value,
+        religion : document.getElementById("religion").value,
         phone : document.getElementById("phone").value,
 
     };
