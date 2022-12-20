d46d489b0bca1992c9c1dd3e9f7097b6f892424d
Avinash Gummadi <gummadi.avinash@indiamart.com>
2022-12-20 20:07:50 +0530

    huge changes

--- js/scripts.js
+++ js/scripts.js
@@ -31,7 +31,35 @@
     })
 
 }) (jQuery)
+// 
+var counter = function() {
+	
+	$('.section-counter').waypoint( function( direction ) {
 
+		if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+
+			var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+			$(this.element).find('.number-counter').each(function(){
+				var $this = $(this),
+					num = $this.data('number');
+				$this.animateNumber(
+				  {
+				    number: num,
+				    numberStep: comma_separator_number_step
+				  }, 
+				  {
+				  	easing: 'swing',
+    				duration: 3000
+				  }
+				);
+			});
+			
+		}
+
+	} , { offset: '95%' } );
+};
+counter();
+// 
 function openform(option) {
     if (parseInt(option) == 1) {
         $('#formID')[0].style.display = 'block';        
